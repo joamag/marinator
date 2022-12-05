4a08880cccffced8636e59d9994277630d543539
João Magalhães <joamag@gmail.com>
2022-12-05 14:45:53 +0000

    feat: removed www

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     description = "The power of a Marina for everyone",
     license = "Apache License, Version 2.0",
     keywords = "ripe api",
-    url = "http://www.joao.me",
+    url = "http://joao.me",
     zip_safe = False,
     packages = [
         "marinator"
